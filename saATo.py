4e57c756600b6ca53b1719948291457da6b82a21
dot_blue <onajafi141@gmail.com>
2018-09-04 22:32:05 +0430

    Got the hang of the Time_zone library

--- saATo.py
+++ saATo.py
@@ -1,4 +1,15 @@
+# import datetime
+# from datetime import tzinfo,timedelta
+
+# print str(datetime.datetime.now().hour) + ":" + str(datetime.datetime.now().minute)
+
+
 import datetime
-
-print str(datetime.datetime.now().hour) + ":" + str(datetime.datetime.now().minute)
-
+import pytz
+# "Asia/Tehran"
+# America/Los_Angeles
+utc_now = pytz.utc.localize(datetime.datetime.utcnow())
+# print utc_now
+# print utc_now.astimezone(pytz.timezone("Asia/Tehran"))
+d_aware = pytz.timezone("Asia/Tehran").localize(datetime.datetime.now())
+print d_aware.astimezone(pytz.timezone("America/Los_Angeles")).time()
