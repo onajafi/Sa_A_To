9b7ede765ad9a241313f4bbc59173c19e7d92544
dot_blue <onajafi141@gmail.com>
2018-09-05 00:18:36 +0430

    Fixed the paused time issue

--- saATo.py
+++ saATo.py
@@ -5,9 +5,9 @@ utc_now = pytz.utc.localize(datetime.datetime.utcnow())
 
 # print utc_now
 # print utc_now.astimezone(pytz.timezone("Asia/Tehran"))
-d_aware = pytz.timezone("Etc/GMT").localize(datetime.datetime.now())
 
 while(True):
+    d_aware = pytz.timezone("Etc/GMT").localize(datetime.datetime.now())
     print d_aware.astimezone(pytz.timezone("Asia/Tehran")).time()
     print isTimePretty(d_aware.astimezone(pytz.timezone("Asia/Tehran")).time())
     time.sleep(30)
