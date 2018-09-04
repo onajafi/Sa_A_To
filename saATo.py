a690d3814c6e3e5697ee067d86df2021d05d4078
dot_blue <onajafi141@gmail.com>
2018-09-05 01:25:06 +0430

    Going for the final tests

--- saATo.py
+++ saATo.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
 import datetime,time
 import pytz
 from prettyTime import isTimePretty
@@ -6,15 +9,25 @@ import telebot,inits
 bot = telebot.TeleBot(inits.tel_token)
 
 
-@bot.message_handler(content_types=['text'])
+@bot.message_handler()
 def send_welcome(message):
     print message
-    bot.send_message(inits.chat_id, "Salam")
+    bot.forward_message(inits.my_id,message.chat.id,message.message_id)
 
 
 while(True):
-    bot.polling()
-    d_aware = pytz.timezone("Etc/GMT").localize(datetime.datetime.now())
-    print d_aware.astimezone(pytz.timezone("Asia/Tehran")).time()
-    print isTimePretty(d_aware.astimezone(pytz.timezone("Asia/Tehran")).time())
-    time.sleep(1)
+    try:
+        bot.polling()
+
+        d_aware = pytz.timezone("Etc/GMT").localize(datetime.datetime.now())
+        print d_aware.astimezone(pytz.timezone("Asia/Tehran")).time()
+        timeIsPretty = isTimePretty(d_aware.astimezone(pytz.timezone("Asia/Tehran")).time())
+        print timeIsPretty
+        if(timeIsPretty):
+            time.sleep(1)
+            bot.send_message(inits.chat_id,"ساعتو")
+    except KeyboardInterrupt:
+        break
+    except:
+        pass
+    time.sleep(5)
