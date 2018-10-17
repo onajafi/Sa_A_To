079741ff32545a580de42c4948c9ca99251b7a33
dot_blue <onajafi141@gmail.com>
2018-10-17 21:00:20 +0330

    Added the inits file

---
+++ inits.py
@@ -0,0 +1,7 @@
+#This file should not be publicly accessible!
+
+tel_token = "<<<YOUR BOT TOKEN HERE>>>"
+# tel_token = ""
+
+chat_id = #the chat or channel serial_ID where the bot will send its notification
+my_id = #Your own user serial_ID in telegram (a positive integer)
